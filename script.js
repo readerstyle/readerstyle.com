2411e433a29ac305045aa70f3cb510de21473e1c
Kyle E. Mitchell <kyle@kemitchell.com>
2019-12-10 00:45:41 -0800

    Script: Set cookie samesite=none

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ function updateStyleButtons (style) {
 }
 
 function setCookie (style) {
-  document.cookie = 'slug=' + style.slug + ';domain=readerstyle.com;secure'
+  document.cookie = 'slug=' + style.slug + ';domain=readerstyle.com;secure;samesite=none'
 }
 
 function isStyleLink (element) {
